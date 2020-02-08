b61e60c576ed8c68171e0d1e218b210890bc7be9
Damien Ostler <damienostler1@outlook.com>
2020-02-08 01:05:27 -0500

    Database connected, templates setup

--- DemoProject/client/main.js
+++ DemoProject/client/main.js
@@ -1,3 +1,2 @@
-import { Template } from 'meteor/templating';
-import './main.html';
 
+import '../imports/ui/body.js'; 
---
+++ DemoProject/imports/ui/body.js
@@ -0,0 +1,9 @@
+import { Template } from 'meteor/templating';
+import './body.html';
+import { Tasks } from '../api/tasks.js';
+ 
+Template.body.helpers({
+  tasks() {
+    return Tasks.find({});
+  }
+});
